b2800331fddd322dea8713b0d8c22c02564e2bd8
vmrchs <vmrchs@gmail.com>
2023-03-07 20:53:59 -0300

    Hold dice function implemented, just missing winning condition pretty much

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,53 @@
 import React from "react";
 import "./App.css";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
 
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      let randomNumber = Math.ceil(Math.random() * 6);
-      newDice.push(randomNumber);
+      newDice.push(generateNewDie());
     }
     // console.log(newDice);
     return newDice;
   }
 
   function rollDice() {
-    setDice(allNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
   }
 
-  const diceElements = dice.map((die) => <Die value={die} />);
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      id={die.id}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
 
   return (
     <main>
